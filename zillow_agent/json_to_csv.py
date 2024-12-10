7101180207f8840cf0f704a69b20e8040b6c9adb
wa1ker38552 <walker.alt.38552@gmail.com>
2024-12-10 09:34:26 -0800

    changed csv structure

--- zillow_agent/json_to_csv.py
+++ zillow_agent/json_to_csv.py
@@ -3,6 +3,14 @@ import json
 
 with open('zillow_agent_data.json', 'r') as json_file, open('data.csv', 'w', newline='') as csv_file:
     data = json.loads(json_file.read())
+    parsed = []
+
+    for item in data:
+        item['listing_1'] = item['last_two_listings'][0]
+        item['listing_2'] = item['last_two_listings'][1]
+        del item['last_two_listings']
+        parsed.append(item)
+
     writer = csv.DictWriter(csv_file, fieldnames=data[0].keys())
     writer.writeheader()
     writer.writerows(data)
